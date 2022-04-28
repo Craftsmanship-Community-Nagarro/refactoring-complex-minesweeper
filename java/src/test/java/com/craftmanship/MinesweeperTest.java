650b2a911e71eef5160e752bcf7d3927189da449
rgerm <roland.germ@admiral.at>
2022-04-28 13:51:27 +0200

    refactor: removed Java test to cleanup starting point

--- java/src/test/java/com/craftmanship/MinesweeperTest.java
+++ java/src/test/java/com/craftmanship/MinesweeperTest.java
@@ -1,81 +1,18 @@
 package com.craftmanship;
 
-import org.approvaltests.Approvals;
 import org.approvaltests.reporters.JunitReporter;
 import org.approvaltests.reporters.UseReporter;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.io.ByteArrayOutputStream;
 import java.io.FileNotFoundException;
-import java.io.PrintStream;
 
 @UseReporter(JunitReporter.class)
 public class MinesweeperTest {
 
     @Test
-    public void shouldSolveBoardWithMultipleBombs() throws FileNotFoundException {
-        int[][] board = new int[][]{
-                {0, 0, 0, 0, 0},
-                {5, 0, 0, 5, 0},
-                {5, 5, 0, 5, 5},
-                {0, 0, 0, 0, 0}
-        };
-
-        String solvedBoard = solvedBoard(board);
-
-        Approvals.verify(solvedBoard);
-    }
-
-    @Test
-    public void solvedBoardWithBombsInCorners() throws FileNotFoundException {
-        int[][] board = new int[][]{
-                {5, 0, 0, 5},
-                {0, 0, 0, 0},
-                {0, 0, 0, 0},
-                {5, 0, 0, 5}
-        };
-
-        String solvedBoard = solvedBoard(board);
-
-        Approvals.verify(solvedBoard);
-    }
-
-    @Test
-    public void solvedBoardWithBombsInDiagonals() throws FileNotFoundException {
-        int[][] board = new int[][]{
-                {5, 0, 0, 5},
-                {0, 5, 5, 0},
-                {0, 5, 5, 0},
-                {5, 0, 0, 5}
-        };
-
-        String solvedBoard = solvedBoard(board);
-
-        Approvals.verify(solvedBoard);
-    }
-
-    @Test
-    public void solvedBoardWithSmallGrid() throws FileNotFoundException {
-        int[][] board = new int[][]{
-                {5, 5},
-                {5, 5}
-        };
-
-        String solvedBoard = solvedBoard(board);
-
-        Approvals.verify(solvedBoard);
-    }
-
-    private String solvedBoard(int[][] board) {
-        Minesweeper.calculateNeighbours(board.length, board[0].length, board);
-
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(out));
-
-        Minesweeper.printOutTheGrid(board.length, board[0].length, board);
-
-        String solvedBoard = out.toString();
-        return solvedBoard;
+    public void shouldTestEnoughToStartRefactoring() throws FileNotFoundException {
+        Assertions.assertThat("Missing tests").isEqualTo("are added");
     }
 
 }
